099aef0b05ef0fdd6f950ad1082576e4f9a248a2
manuntag <manuntag@ualberta.ca>
2015-01-14 16:30:11 -0800

    Initial Commit

---
+++ LinkedLists/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedLists
+//
+//  Created by David Manuntag on 2015-01-14.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
